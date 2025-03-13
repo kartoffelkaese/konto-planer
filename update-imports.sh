45fd3580f15d8ffcd7047fcf0de89645e643b1f9
Martin Urban <github@gcal.ch>
2025-03-13 19:13:50 +0100

    2.10.2

--- update-imports.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Finde alle TypeScript-Dateien im API-Verzeichnis
-find src/app/api -name "*.ts" -type f -exec sed -i '' 's|from '\''@/app/api/auth/\[...nextauth\]/route'\''|from '\''@/lib/auth'\''|g' {} + 

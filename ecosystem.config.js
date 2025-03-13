d5dce49ca8b301ddfbe3fb13d9976e267d5a4bb4
Martin Urban <github@gcal.ch>
2025-03-13 16:38:04 +0100

    2.8.0

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
       instances: 1,
       autorestart: true,
       watch: false,
-      max_memory_restart: '512M',
+      max_memory_restart: '128M',
       env_production: {
         NODE_ENV: 'production'
       }
